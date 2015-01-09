24d25e21b4f87b81830b4bccfbdcc4ce00a913fc
enlightendev <juan.lamadrid@gmail.com>
2015-01-09 12:01:55 -0500

    remove yeoman generated defaults that are not needed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,7 @@
  */
 angular.module('meanTraderApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.test = 'Hello Angular';
+
   });
