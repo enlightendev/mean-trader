dcc0adeb96a121540f6bc4581b9c637824c22081
enlightendev <juan.lamadrid@gmail.com>
2015-01-09 09:20:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+## create steps
+
+1 - created folder mean-trader
+2 - executed `yo angular mean-trade`
+3 - updated bower.json to use angular 1.3 - `bower install`
+4 - 
+
+
+
+## running
---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meanTraderApp
+ * @description
+ * # meanTraderApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('meanTraderApp', [
+    'ngResource'
+  ]);
